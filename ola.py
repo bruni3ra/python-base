3969339ffe1afd6392e7a410b6852655e62e9a3b
Igor Bruniera <igor.bruniera@escola.pr.gov.br>
2026-02-09 14:11:19 +0000

    Aula01

---
+++ ola.py
@@ -0,0 +1,8 @@
+#nome = input("Digite seu nome: ")
+#print("Olá, ", nome , "Seja bem vindo")
+
+nome = input("Digite seu nome: ")
+print(nome)
+
+sobrenome = input("Digite seu sobrenome: ")
+print("Olá,",nome, sobrenome , "seja bem vindo")
